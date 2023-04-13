1eb1d8fc90ebbc4f8dcc6db1077abbc8da514edf
Spearsona <c17753589@mytudublin.ie>
2023-04-13 13:34:30 +0100

    Recreated Project With custom user model

---
+++ ATPassport/webportal/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+# Create your models here.
+class AtUser(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key= True)
+
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    dob = models.DateField('Date Of Birth')
+    email = models.EmailField
+
+    def __str__(self):
+        return self.firstName + " " + self.lastName 
+
+#Single Provider user account will be linked to each Loan. 
+class Provider(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key= True)
+    name = models.CharField(max_length=100)
+
+    #Could potentially make this contain a list of staff users as well as part of group permissions. 
+
+    def __str__(self):
+        return self.name
+
+class AtCategory(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    atcategory = models.ForeignKey(AtCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class LoanInstance(models.Model):
+    due_date = models.DateField()
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    atuser = models.ForeignKey(AtUser, on_delete=models.CASCADE)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = (("can_return", "Can set loan as returned"),)
+
